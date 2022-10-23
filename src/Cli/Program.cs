6771593473364542ea8be250865a959a4e39ab03
Nikita C <cethrivias@gmail.com>
2022-10-23 11:45:24 +0200

    Created client and bson insert

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+// Connecting to MongoDB server
+// Can be registered as a signleton in your DI container
+var client = new MongoClient("mongodb://root:testdbpass@127.0.0.1:27017/admin");
+var database = client.GetDatabase("cip-mongodb");
+var collection = database.GetCollection<BsonDocument>("Events");
+
+await collection.InsertOneAsync(new BsonDocument()
+{
+    { "_id", ObjectId.GenerateNewId() },
+    { "Title", "Ted Heeran concert" }
+});
+
+Console.WriteLine("Done!");
